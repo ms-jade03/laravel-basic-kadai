22fd803bc7f0f2025102e1c7865afe1a44924d80
ms-jade03 <ms_jade_03_0501@yahoo.co.jp>
2024-09-13 20:24:30 +0900

    web.phpを編集

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +30,11 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/posts',[PostController::class, 'index']);
+
+Route::post('posts/store', [PostController::class, 'store'])->name('posts.store');
+
+Route::get('/posts/create', [PostController::class, 'create']);
+
+Route::get('/posts/{id}', [PostController::class, 'show']);
